6e62c0c545b211706572ba8a546e50c17a72c808
Miguel Julio <mjulio@artnet.com>
2018-09-24 11:11:05 -0400

    add static resources and use renderer package

--- server.go
+++ server.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	"github.com/thedevsaddam/renderer"
 )
 
 type page struct {
@@ -12,22 +15,39 @@ type page struct {
 	Copy   string
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>This is Go Server!</h1>")
-	fmt.Fprintf(w, "<p>go is simple as well</P>")
+var opts = renderer.Options{
+	ParseGlobPattern: "./templates/*.html",
 }
+var rnd = renderer.New(opts)
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	p := page{Title: "about", Header: "What About it", Copy: "This the copy for the about page."}
-	t, _ := template.ParseFiles("basictemplate.html")
-	err := t.Execute(w, p)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("\nroute at %v", r.RequestURI)
+	d := page{Title: "home", Header: "What About home", Copy: "This the copy for the about page."}
+	err := rnd.HTML(w, http.StatusOK, "layout", d)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	p := page{Title: "about", Header: "What About it", Copy: "This the copy for the about page."}
+	t, _ := template.ParseFiles("./templates/index.html")
+	err := t.Execute(w, p)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":8000", nil)
+	port := ":8000"
+	mux := http.NewServeMux()
+	// setup to serve static assets: css, js and images
+	fs := http.StripPrefix("/static", http.FileServer(http.Dir("./static")))
+
+	mux.Handle("/static/", fs)
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/about", aboutHandler)
+	log.Printf("\nlistenting at %v", port)
+	http.ListenAndServe(":8000", mux)
 }
